e1e89679b9c61f32c3c1928b963d1c9f90e61f73
snelluri <swathi.nelluri@trinet.com>
2018-06-18 15:27:57 +0530

    Login

---
+++ src/main/java/com/Hello/Login/LoginDetails.java
@@ -0,0 +1,23 @@
+package com.Hello.Login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginDetails {
+	
+	@Test
+	
+	public void initBrowser(){
+		
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "D:\\SeleniumPractice\\Login\\src\\main\\java\\drivers\\chromedriver.exe");
+driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.google.com/");
+System.out.println("Hello");
+}
+	
+	
+
+}
