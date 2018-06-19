25c9ef7ce83e16cae12b27ed021605a95f748f11
snelluri <swathi.nelluri@trinet.com>
2018-06-19 13:17:43 +0530

    update code pom

--- src/main/java/com/Hello/Login/LoginDetails.java
+++ src/main/java/com/Hello/Login/LoginDetails.java
@@ -5,19 +5,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class LoginDetails {
-	
+
 	@Test
-	
-	public void initBrowser(){
-		
+	public void initBrowser() {
+
 		WebDriver driver;
-		System.setProperty("webdriver.chrome.driver", "D:\\SeleniumPractice\\Login\\src\\main\\java\\drivers\\chromedriver.exe");
-driver=new ChromeDriver();
-driver.manage().window().maximize();
-driver.get("https://www.google.com/");
-System.out.println("Hello");
-}
-	
-	
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\SeleniumPractice\\Login\\src\\main\\java\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		System.out.println("Hello");
+	}
 
 }
